05931f43b3d31a4da36a29385dec2c3d35a39a0b
ram <ramthatigutla@gmail.com>
2023-08-11 19:15:35 +0530

    ngw

--- main.tf
+++ main.tf
@@ -44,16 +44,17 @@ module "rabbitmq" {
 
 }
 
-#module "rds" {
-#  source = "git::https://github.com/Ramthatigutla/tf-module-rds.git"
-#
-#  for_each       = var.rds
-#  component      = each.value["component"]
-#  engine         = each.value["engine"]
-#  engine_version = each.value["engine_version"]
-#  db_name        = each.value["db_name"]
-#  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
-#
-#  tags       = var.tags
-#  env        = var.env
-# }
+module "rds" {
+  source = "git::https://github.com/Ramthatigutla/tf-module-rds.git"
+
+  for_each       = var.rds
+  component      = each.value["component"]
+  engine         = each.value["engine"]
+  engine_version = each.value["engine_version"]
+  db_name        = each.value["db_name"]
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+
+  tags       = var.tags
+  env        = var.env
+  kms_key_id = var.kms_key_id
+ }
